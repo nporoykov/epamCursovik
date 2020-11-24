2e4c38ff2428d582f9c0707c0a3f7726a7cfbb76
nporoykov <nporoykov@gmail.com>
2020-11-24 23:38:08 +0300

    added trustButton click

--- src/test/java/pages/MajorPage.java
+++ src/test/java/pages/MajorPage.java
@@ -24,9 +24,22 @@ public class MajorPage extends BaseClass{
     @FindBy(xpath="//a[text()='Video']") //локатор event tab'а
     private WebElement videoTab;
 
+    @FindBy(xpath="//div[@id='onetrust-policy']") //локатор event tab'а
+    private WebElement trustButton;
+
     public MajorPage(WebDriver driver) {
         super(driver);
         PageFactory.initElements(driver, this);
+    }
+
+    public void clickTrustButton(){
+        try{
+            waitForElement(trustButton).click();
+            logger.info("Нажимаем на 'Принять coockies'");
+
+        }catch(Exception e){
+          //  logger.info("Нет кнопки 'Принять coockies'");
+        }
     }
 
     @Step("Открыта страница \"+url")
@@ -39,6 +52,7 @@ public class MajorPage extends BaseClass{
 
     @Step("Переходим на вкладку EVENTS")
     public EventsPage clickEventsTab(){
+        clickTrustButton();
         waitForElement(eventTab).click();
         logger.info("Переходим на вкладку EVENTS");
 
@@ -47,6 +61,7 @@ public class MajorPage extends BaseClass{
 
     @Step("Переходим на вкладку VIDEO")
     public VideoPage clickVideoTab(){
+        clickTrustButton();
         waitForElement(videoTab).click();
         logger.info("Переходим на вкладку VIDEO");
 
